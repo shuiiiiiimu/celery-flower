833c5c8609714d6679ead65b8d2f5256fa5add52
zhangsm <shuimu625@gmail.com>
2017-01-05 11:47:57 +0800

    多种定时任务方式

--- task.py
+++ task.py
@@ -2,13 +2,14 @@
 
 from celery import Celery
 from celery.task import periodic_task
+from celery.schedules import crontab
 from datetime import timedelta
 import time 
 
 broker = 'redis://{0}:{1}/5'.format('127.0.0.1', 6379)
 backend = 'redis://{0}:{1}/6'.format('127.0.0.1', 6379)
 
-app = Celery('tasks', broker=broker, backend=backend)
+app = Celery('task', broker=broker, backend=backend)
 
 
 @app.task()
@@ -20,7 +21,8 @@ def add(x, y):
 
 @app.task()
 def test(x, y):
-    time.sleep(100)
+    time.sleep(5)
+    print('----> test')
     return x + y
 
 
@@ -28,3 +30,29 @@ def test(x, y):
 def newfeeds():
     print('-------> newfeeds')
     return 'hello'
+
+
+CELERYBEAT_SCHEDULE = {
+    'test-every-monday-morning': {
+        'task': 'task.test',
+        'schedule': crontab(hour=7, minute=30, day_of_week=1),
+        'args': (6, 5),
+    },
+    'test-every-30-seconds': {
+        'task': 'task.test',
+        'schedule': timedelta(seconds=30),
+        'args': (26, 45),
+    },
+}
+
+CELERY_CONFIG = {
+    'CELERY_TIMEZONE': 'Asia/Shanghai',
+    'CELERY_ENABLE_UTC': True,
+    'CELERY_TASK_SERIALIZER': 'json',
+    'CELERY_RESULT_SERIALIZER': 'json',
+    'CELERY_ACCEPT_CONTENT': ['json'],
+    'CELERYD_MAX_TASKS_PER_CHILD': 1,
+    'CELERYBEAT_SCHEDULE': CELERYBEAT_SCHEDULE     # 启动beat，传入相关参数.
+}
+
+app.conf.update(**CELERY_CONFIG)
